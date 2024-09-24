99cd35b6fb056c5aa45bd4a4069289f73bc93420
akttamanna <Akther-T2@ulster.ac.uk>
2024-09-24 15:32:02 +0100

    Implement program for code

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//int firstNumber = 5;
+
+//int secondNumber = 20;
+
+//int result = firstNumber + secondNumber;
+
+//Console.WriteLine("The addition of the 2 numbers is {0}" , result);
+//Console.ReadKey();
+
+
+//First Number taken from user
+
+Console.WriteLine("Type in the first number followed by the Enter key");
+int firstNumber =  Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type in the second number followed by the Enter key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//Calculation result
+int result = firstNumber + secondNumber;
+
+Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
+
+Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
